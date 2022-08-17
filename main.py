3a36115c8bab81fbd2abd997bcc64ce8c9ef0310
KirillKrasyuk1 <jopaostera@gmail.com>
2022-08-17 19:10:44 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import telebot
+import time
+import requests
+from bs4 import BeautifulSoup as BS
+import schedule
+
+
+bot = telebot.TeleBot('5502855452:AAEDWiH4cF6PQHaVMPV0hOY5cC-9zLhuY5E')
+#TODO менять времябез попадания в функцию answer
+@bot.message_handler(commands=['settings'])
+def settings(message):
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = 'рассылка с погодой'
+    button2 = 'рыссылка с курсом валют'
+    keyboard.add(button1, button2)
+    bot.send_message(message.chat.id, 'Выберете ту функцию которую хотите изменить.')
+
+
+@bot.message_handler(commands=['start'])
+def greeting(message):
+
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = 'погода'
+    button2 = 'курс валют'
+    # button3 = 'быстроновости'
+    keyboard.add(button1, button2)
+    bot.send_message(message.chat.id,
+                     'Привет, я твой умный помощник AS1E! Я умею показывать погоду на сегодня, курс валют и быстроновости.'
+                     ' Выбери что из этого ты хочешь получать каждый день. P.S. чтобы увидеть список команд напиши /help.',
+                     reply_markup=keyboard)
+
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    global id_to_test
+    id_to_test = message.chat.id
+    if message.text == 'погода':
+        bot.send_message(message.chat.id, 'Напииште название свего города через восклицательный знак')
+    if message.text[0] == '!':
+        Text = message.text
+        city = Text[1:]
+        bot.send_message(message.chat.id, 'Отлично, я запомню. Теперь я буду присылать прогноз погоды в 18:20')
+        response = requests.get(f'https://sinoptik.ua/погода-{city.lower()}')
+        html_response = BS(response.content, 'html.parser')
+        a = html_response.find('div', id='bd1')
+        if a == None:
+            bot.send_message(message.chat.id, 'Такого города не существует или его название написано неправильно')
+        else:
+            a = a.select('.temperature')[0].text
+            bot.send_message(message.chat.id, a)
+
+# тестирование таймера
+# def weather():
+#     city = 'самара'
+#     chat_id = id_to_test
+#     response = requests.get(f'https://sinoptik.ua/погода-{city.lower()}')
+#     html_response = BS(response.content, 'html.parser')
+#     a = html_response.find('div', id='bd1')
+#     if a == None:
+#         bot.send_message(chat_id, 'Такого города не существует или его название написано неправильно')
+#     else:
+#         a = a.select('.temperature')[0].text
+#         bot.send_message(chat_id, a)
+#
+#
+# schedule.every().day.at("18:39").do(weather)
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
+
+bot.polling(none_stop=True)
+
+# if button2 == 'курс валют':
+#     bot.send_message(message.chat.id, 'Отлично, теперь я буду присылать тебе курс основых валют')
+# bot.send_message(message.chat.id, 'Хочешь воспользоваться другими моими функциями')
+
